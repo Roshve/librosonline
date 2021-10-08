6be82ca5362c6586b6128d790caa6582ef272e5c
juan <juan@gmail.com>
2021-10-08 06:35:32 -0300

    version 1.2.1 Unidad 2 nivel inicial - Patron MTV

--- vistaprevia/admin.py
+++ vistaprevia/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from vistaprevia.models import Categoria
+from vistaprevia.models import Producto
 
-# Register your models here.
+class ProductoAdmin(admin.ModelAdmin):
+    fields =['categoria', 'fecha_publicacion', 'producto', 'imagen']
+
+admin.site.register(Producto, ProductoAdmin)
+
+
+
+admin.site.register(Categoria)
+#admin.site.register(Producto)
+
